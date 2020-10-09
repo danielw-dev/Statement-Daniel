82239063b457a1891309a961a9f3e0a4ed12d284
Daniel Worthington <me@danielworthington.co.uk>
2020-10-10 00:14:39 +0100

    Update: Navigation / Carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,57 @@
 var mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
   direction: "horizontal",
-  loop: true,
+  loop: false,
+  slidesPerView: 1,
+  centeredSlides: true,
+  grabCursor: true,
+  simulateTouch: true,
+  effect: "coverflow",
 
-  // If we need pagination
+  pagination: true,
   pagination: {
     el: ".swiper-pagination",
   },
 
-  // Navigation arrows
+  coverflowEffect: {
+    rotate: 0,
+    slideShadows: false,
+    depth: 200,
+    stretch: -20,
+  },
+
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      initialSlide: 0,
+    },
+    1024: {
+      slidesPerView: 3,
+      initialSlide: 1,
+      pagination: false,
+    }
+  }
+});
+
+// Mobile Navigation
+var burgerMenu = document.getElementById('burgerMenu');
+var navigation = document.getElementById('navigation');
+
+console.log();
+burgerMenu.addEventListener('click', function(){
+  if(navigation.classList.contains("show")){
+    navigation.classList.remove('show');
+    navigation.classList.add('hide');
+    burgerMenu.children[0].src = "./assets/svg/menu.svg";
+    burgerMenu.children[0].classList.toggle('active');
+  } else {
+    navigation.classList.remove('hide');
+    navigation.classList.add('show');
+    burgerMenu.children[0].src = "./assets/svg/cross.svg";
+    burgerMenu.children[0].classList.toggle('active');
+  }
 });
